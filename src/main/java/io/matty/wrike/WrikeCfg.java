cd180a860d60b0bd422fe66191e95960a926d330
Uki D. Lucas <UkiDLucas@gmail.com>
2016-05-05 14:07:38 -0500

    pulling secret values from local.properties

--- src/main/java/io/matty/wrike/WrikeCfg.java
+++ src/main/java/io/matty/wrike/WrikeCfg.java
@@ -1,7 +1,30 @@
 package io.matty.wrike;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class WrikeCfg {
-    public static final String clientID = "";
-    public static final String clientSecret = "";
-    public static final String callbackUri = "";
+
+    public static String clientID = "";
+    public static String clientSecret = "";
+    public static String callbackUri = "";
+
+    public WrikeCfg() {
+        // get local.properties with your secret stuff
+        // https://developers.wrike.com/getting-started/
+        try {
+            Properties propeties = new Properties();
+            FileInputStream in = null;
+            in = new FileInputStream("local.properties");
+            propeties.load(in);
+            this.clientID = propeties.getProperty("clientID");
+            this.callbackUri = propeties.getProperty("callbackUri");
+            this.clientSecret = propeties.getProperty("clientSecret");
+            in.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
 }
